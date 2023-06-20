7d75760141c456148850b7b52c2bc3c7e083ff8f
Will Gallagher <100447981+wgallagher44@users.noreply.github.com>
2023-06-20 14:02:21 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import { Button,TextField,Stack,List,ListItem } from '@mui/material';
-
+import { useState } from 'react';
 function App() {
+  const [error_cof,setErrorCof] = useState(false);
+  const [helperText_cof,setCofHelperText] = useState("");
+  const [error_pass,setErrorPass] = useState(false);
+  const [helperText_pass,setPassHelperText] = useState("");
     const passwordCheck = (e) =>{
       const currentValue = e.target.value;
       styleCondition(1,/(?=.{8,})/,currentValue)
@@ -22,13 +26,52 @@ function App() {
     }
 
     const resetPassword = () =>{
-      
+      let password = document.getElementById("password")
+      let cof_password = document.getElementById("cof_password");
+      const passwordCheck = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
+      if(password.value != cof_password.value){
+        setErrorCof(true);
+        setCofHelperText("Passwords Don't Match Try Again")
+       cof_password.value = "";
+      }else if(!password.value.match(passwordCheck)){
+        setErrorPass(true);
+        setPassHelperText("Password is invalid please try again");
+        password.value = "";
+      }else{
+        var url = window.location.href;
+        console.log(url);
+        let count = 0;
+        let email = "";
+        for(let i = 0; i < url.length; i++){
+            if(url.charAt(i) == '='){
+              count++;
+              break;
+            }else{
+              count++;
+            }
+        }
+        for(let i = count; i < url.length; i++){
+            email += url.charAt(i);
+        }
+        var data = {email:email,newPassword:password.value}
+        fetch('http://localhost:8000/ResetPassword/',{
+          method:'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }).then(response => response.json())
+        .then(responseData =>{
+         alert(responseData.message)
+        })
+      }
+    
     }
   return (
     <div className="App">
       <Stack spacing={4}>
-        <TextField id = "password" label = "Password" onInput={(event) => passwordCheck(event)}></TextField>
-        <TextField id = "cof_password" label = "Confirm Password"></TextField>
+        <TextField id = "password" label = "Password" type = "password" onInput={(event) => passwordCheck(event)} error = {error_pass} helperText = {helperText_pass}></TextField>
+        <TextField id = "cof_password" label = "Confirm Password" type = "password" error ={error_cof} helperText = {helperText_cof}></TextField>
         <List sx={{ listStyleType: 'disc' }} id = "list-item">
             <ListItem id= "list-item1"    sx={{ padding: 0,textAlign: "center",listStyleType: "disc",display: "list-item",
     }} > At Least 8 Characters </ListItem>
@@ -41,7 +84,7 @@ function App() {
      <ListItem id= "list-item5"   sx={{ padding: 0,textAlign: "center",listStyleType: "disc",display: "list-item",
     }} >At Least One Number </ListItem>
            </List>
-        <Button>Reset Password</Button>
+        <Button onClick={resetPassword}>Reset Password</Button>
       </Stack>
 
     </div>
